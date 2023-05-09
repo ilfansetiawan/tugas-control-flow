474ff25c7d4ab25e8a0d7ba039dde6f7d67f2ea5
ilfan-setiawan <ilfansetiawan82@gmail.com>
2023-05-10 00:27:30 +0700

    deleted coment

--- index.js
+++ index.js
@@ -12,25 +12,3 @@ if (nilai >= 80 && nilai <= 100) {
   alert("Nilai anda E");
 }
 
-// <script>
-//      function konversi(){
-//         var nilai_a = Number(document.getElementById("a").value);
-//         var nilai_huruf;
-
-//         if((nilai_a>90)&&(nilai_a<=100))
-//             nilai_huruf = 'A';
-//         else if((nilai_a>80)&&(nilai_a<=90))
-//             nilai_huruf = 'B+';
-//         else if((nilai_a>70)&&(nilai_a<=80))
-//             nilai_huruf = 'B';
-//         else if((nilai_a>65)&&(nilai_a<=70))
-//             nilai_huruf = 'C+';
-//         else if((nilai_a>59)&&(nilai_a<=65))
-//             nilai_huruf = 'C';
-//         else if((nilai_a>49)&&(nilai_a<=59))
-//             nilai_huruf = 'D';
-//         else
-//             nilai_huruf = 'E';
-//         document.getElementById("c").value = nilai_huruf;
-// }    
-// </script>
